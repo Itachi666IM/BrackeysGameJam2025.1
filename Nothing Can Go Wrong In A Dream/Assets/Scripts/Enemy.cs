556428ec163bf675bc2a003617f6136cf156e73b
Kisame712 <kisamehoshigakiuhmw712@gmail.com>
2025-02-18 01:00:23 +0530

    added enemy and changed folder structure

---
+++ Nothing Can Go Wrong In A Dream/Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    Rigidbody2D enemyRb;
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        enemyRb.velocity = new Vector2(moveSpeed, 0f);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        moveSpeed = -1 * moveSpeed;
+        FlipEnemy();
+    }
+
+    void FlipEnemy()
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(enemyRb.velocity.x)), 1f);
+    }
+}
