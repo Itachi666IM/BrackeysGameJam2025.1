cacab22758f270efffc8f37c710cad722403af17
Sanj1t <79507387+Itachi666IM@users.noreply.github.com>
2025-02-17 21:39:03 +0530

    Added Intro Cut Scene

--- Nothing Can Go Wrong In A Dream/Assets/Scripts/MenuManager.cs
+++ Nothing Can Go Wrong In A Dream/Assets/Scripts/MenuManager.cs
@@ -2,16 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MenuManager : MonoBehaviour
 {
+    public GameObject buttons;
+    public TMP_Text title;
+    public GameObject cutScene;
     public void Play()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        buttons.SetActive(false);
+        title.gameObject.SetActive(false);
+        cutScene.SetActive(true);
+        Invoke(nameof(StartGame), 13f);
     }
 
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
